2933ec8bd1c760a8e327c0e059c15e68fe76649f
gharibj6942 <gharibj6942@CA-SU-F106-22.sduhsd.lan>
2019-03-19 09:02:41 -0700

    Grid Done

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -3,32 +3,70 @@ package Grid;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
+public class Grid {
 
-public class Grid
-{
-   private String[][] grid;
+    private String[][] grid;
 
-   //load vals into the rows x cols grid randomly
-	public Grid(int rows, int cols, String[] vals)
-	{
-	}
+    //load vals into the rows x cols grid randomly
+    public Grid(){
+    }
+    public Grid(String[] vals) {
+         int rows = vals.length;
+         int cols = vals.length;
+        grid = new String[rows][cols];
+        int loc = 0;
+        for(int i = 0; i < grid.length; i++){
+            for(int n = 0; n < grid[i].length; n++){
+                loc = (int)Math.floor(Math.random()*vals.length);
+                grid[i][n] = vals[loc];
+            }
+        }
+        
+        
+        
+    }
 
-	//find out which of the vals occurs the most
-	public String findMax(String[] vals)
-	{
-		return "nothing yet";
-	}
+    //find out which of the vals occurs the most
+    public String findMax(String[] vals, int[] valNums) {
+         int maxVal = 0; 
+         for( int i = 0; i < valNums.length; i++){
+             maxVal = Math.max(maxVal, valNums[i]);
+         }
+         for( int n = 0; n < valNums.length; n++){
+             if (valNums[n] == maxVal){
+                 maxVal = n;
+             }
+         }
+        return (vals[maxVal] + " occured the most at " + valNums[maxVal] + " times.");
+    }
 
-	//returns a count of how many times val occurs in the matrix
-	private int countVals( String val )
-	{
-		return 0;
-	}
+    //returns a count of how many times val occurs in the matrix
+    public int[] countVals(String[] val) {
+        String valAt = "";
+        int[] valCount = new int[val.length];
+        for(int j = 0; j < val.length; j++){
+            valAt = val[j];
+            for(int i = 0; i < grid.length; i++){
+                for(int n = 0; n < grid[i].length; n++){
+                    if (grid[i][n] == valAt){
+                        valCount[j]++;
+                    }
+                }
+            }
+        }
+        return valCount;
+    }
 
-	//display the grid
-	public String toString()
-	{
-		String output="";
-		return output;
-	}
+    //display the grid
+    public String gridPrint(){
+        String output = "";
+        for(int i = 0; i < grid.length; i++){
+            for(int n = 0; n < grid[i].length-1; n++){
+                output += grid[i][n] + " ";
+            }
+            output += grid[i][grid[i].length-1];
+            output+= "\n";
+        }
+        return output;
+    }
 }
--- src/Grid/GridRunner.java
+++ src/Grid/GridRunner.java
@@ -3,15 +3,25 @@ package Grid;
 //(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
-
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class GridRunner
-{
-	public static void main( String args[] ) throws IOException
-	{
-	}
+public class GridRunner {
+
+    public static void main(String args[]) throws IOException {
+        String[] vals = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+        
+        Grid graph = new Grid(vals);
+        String out = graph.gridPrint();
+        System.out.println(out);
+        int[] valNums =graph.countVals(vals);
+        
+        for(int i = 0; i < vals.length; i++){
+            System.out.println(vals[i] + " appeared " + valNums[i] + " times.");
+        }
+        System.out.println();
+        System.out.println(graph.findMax(vals, valNums));
+    }
 }
